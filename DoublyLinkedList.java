80b44c5c17d2dec32725ca663a6768c72d9e0f53
Sasindi891 <hewage.sasindi@gmail.com>
2025-12-31 00:14:55 +0530

    Create DoublyLinkedList.java

---
+++ DoublyLinkedList.java
@@ -0,0 +1,42 @@
+public class DoublyLinkedList {
+
+    class Node {
+        int data;
+        Node prev, next;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    Node head;
+
+    void insert(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null)
+            temp = temp.next;
+        temp.next = newNode;
+        newNode.prev = temp;
+    }
+
+    void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " <-> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        DoublyLinkedList list = new DoublyLinkedList();
+        list.insert(5);
+        list.insert(10);
+        list.display();
+    }
+}
