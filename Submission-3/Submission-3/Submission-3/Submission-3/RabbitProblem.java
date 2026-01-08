4afd59c8c350ef0c9d602fa28083aaef2e039565
Sasindi891 <hewage.sasindi@gmail.com>
2026-01-08 23:01:48 +0530

    Create RabbitProblem.java

---
+++ Submission-3/Submission-3/Submission-3/Submission-3/RabbitProblem.java
@@ -0,0 +1,13 @@
+public class RabbitProblem {
+
+    static int rabbits(int n) {
+        if (n == 0 || n == 1)
+            return 1;
+        return rabbits(n - 1) + rabbits(n - 2);
+    }
+
+    public static void main(String[] args) {
+        int months = 6;
+        System.out.println("Rabbit pairs after " + months + " months: " + rabbits(months));
+    }
+}
