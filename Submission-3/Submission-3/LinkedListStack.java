a28883061faaeb0174806f344e1bec7ea4a6870a
Sasindi891 <hewage.sasindi@gmail.com>
2026-01-08 15:14:45 +0530

    Create LinkedListStack.java

---
+++ Submission-3/Submission-3/LinkedListStack.java
@@ -0,0 +1,36 @@
+public class LinkedListStack {
+
+    class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    Node top = null;
+
+    void push(int data) {
+        Node newNode = new Node(data);
+        newNode.next = top;
+        top = newNode;
+    }
+
+    int pop() {
+        if (top == null) {
+            System.out.println("Stack is empty");
+            return -1;
+        }
+        int value = top.data;
+        top = top.next;
+        return value;
+    }
+
+    public static void main(String[] args) {
+        LinkedListStack s = new LinkedListStack();
+        s.push(10);
+        s.push(20);
+        System.out.println(s.pop());
+    }
+}
