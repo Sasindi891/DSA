95e758242774323f20bfea3f041519f95b8c3878
Sasindi891 <hewage.sasindi@gmail.com>
2026-01-10 10:39:36 +0530

    Add BubbleSort class with sorting and printing methods
    Implement Bubble Sort algorithm and print method

---
+++ Submission-4/Submission-4/BubbleSort.java
@@ -0,0 +1,28 @@
+public class BubbleSort {
+
+    static void bubbleSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    static void print(int[] arr) {
+        for (int x : arr)
+            System.out.print(x + " ");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 3, 8, 2, 1};
+        bubbleSort(arr);
+        print(arr);
+    }
+}
